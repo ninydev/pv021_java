8349beef3615b6c071c1c416a963f94e75dcaa1b
Oleksandr Nykytin <keeper@ninydev.com>
2022-08-16 19:05:23 +0300

    Welcome Java

---
+++ _01_welcome/run.cmd
@@ -0,0 +1,4 @@
+C:\Users\keepe\.jdks\openjdk-18.0.2\bin\java.exe
+"-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2\lib\idea_rt.jar=58628:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2\bin"
+-Dfile.encoding=UTF-8
+-classpath C:\Users\keepe\repos\pv021_java\_01_welcome\out\production\_01_welcome Main
