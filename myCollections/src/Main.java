fcacbabac7e10e7268c403c912712db82942f075
Oleksandr Nykytin <keeper@ninydev.com>
2022-08-18 19:23:59 +0300

    Array

--- myCollections/src/Main.java
+++ myCollections/src/Main.java
@@ -1,5 +1,16 @@
+import entities.Contact;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MyArray work = new MyArray();
+        work.run();
+
+//        Contact sasha = new Contact("Sasha", "+380965747708");
+//        System.out.println(sasha);
+
+        System.out.println("Bye Bye");
     }
+
+
 }
---
+++ myCollections/src/MyArray.java
@@ -0,0 +1,26 @@
+import entities.Contact;
+
+public class MyArray implements Runnable
+{
+    @Override
+    public void run() {
+        Contact val = new Contact("Valentin",  "+38063");
+        Contact svet = new Contact("Svetlana", "+38067");
+
+        Contact[] myFriends = new Contact[2];
+        myFriends[0] = val;
+        myFriends[1] = svet;
+
+        System.out.println("My Friends");
+        for(int i = 0; i < 2; i++) {
+            System.out.println(myFriends[i]);
+        }
+
+
+
+
+
+
+
+    }
+}
