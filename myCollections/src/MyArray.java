44121cd0773e060109f2944f2b05b2aa0a2981c2
Oleksandr Nykytin <keeper@ninydev.com>
2022-08-18 19:29:57 +0300

    Array

--- myCollections/src/MyArray.java
+++ myCollections/src/MyArray.java
@@ -4,6 +4,16 @@ public class MyArray implements Runnable
 {
     @Override
     public void run() {
+
+        try {
+            simpleArray();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private void simpleArray() throws Exception
+    {
         Contact val = new Contact("Valentin",  "+38063");
         Contact svet = new Contact("Svetlana", "+38067");
 
@@ -15,12 +25,6 @@ public class MyArray implements Runnable
         for(int i = 0; i < 2; i++) {
             System.out.println(myFriends[i]);
         }
-
-
-
-
-
-
 
     }
 }
