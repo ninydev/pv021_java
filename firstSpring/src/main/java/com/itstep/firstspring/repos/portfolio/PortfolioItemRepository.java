f85910e9fdebadc119b20d9ac5de2dcf65dd2588
Oleksandr Nykytin <keeper@ninydev.com>
2022-09-21 20:44:07 +0300

    Portfolio Many to Many

--- firstSpring/src/main/java/com/itstep/firstspring/repos/portfolio/PortfolioItemRepository.java
+++ firstSpring/src/main/java/com/itstep/firstspring/repos/portfolio/PortfolioItemRepository.java
@@ -1,10 +1,15 @@
 package com.itstep.firstspring.repos.portfolio;
 
+import com.itstep.firstspring.entities.portfolio.PortfolioCategory;
 import com.itstep.firstspring.entities.portfolio.PortfolioItem;
+import com.itstep.firstspring.entities.portfolio.PortfolioTag;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PortfolioItemRepository extends CrudRepository<PortfolioItem, Long>
 {
+
 }
