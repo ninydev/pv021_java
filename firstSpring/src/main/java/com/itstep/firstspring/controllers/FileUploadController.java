f0c6bf1b40b7df8d8d3442ab26c18c1eb2d593ab
Oleksandr Nykytin <keeper@ninydev.com>
2022-09-27 20:33:33 +0300

    Resize Image and Convert to WebP

--- firstSpring/src/main/java/com/itstep/firstspring/controllers/FileUploadController.java
+++ firstSpring/src/main/java/com/itstep/firstspring/controllers/FileUploadController.java
@@ -1,9 +1,11 @@
 package com.itstep.firstspring.controllers;
 
-
 import com.itstep.firstspring.exceptions.storage.StorageFileNotFoundException;
 import com.itstep.firstspring.services.storage.StorageService;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -27,6 +29,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.imageio.ImageIO;
+
 @Controller
 public class FileUploadController {
 
@@ -35,6 +39,28 @@ public class FileUploadController {
     @Autowired
     public FileUploadController(StorageService storageService) {
         this.storageService = storageService;
+    }
+
+    @GetMapping("/test_webp")
+    public String convertTest()
+    {
+        try {
+            //Read JPG image
+            BufferedImage image = ImageIO.read(new File("C:/Temp/f.jpeg"));
+
+            BufferedImage resizedImage = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics2D = resizedImage.createGraphics();
+            graphics2D.drawImage(image, 0, 0, 100, 100, null);
+            graphics2D.dispose();
+
+            // Encode it as webp using default settings and save it as webp file
+            ImageIO.write(resizedImage, "webp", new File("C:/Temp/out.webp"));
+        }
+        catch (Exception ex) {
+            return ex.getMessage();
+        }
+
+        return "Ok";
     }
 
     @GetMapping("/files")
@@ -52,7 +78,12 @@ public class FileUploadController {
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
+
         Resource file = storageService.loadAsResource(filename);
+
+        // WebpEncoder en = new WebpEncoder();
+        //WebpEncoder.encode(originImageFilePath,webpImageFilePath);
+
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
                 "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
